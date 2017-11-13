d0f70d931c5f647bfad90c23a3ce55123286c540
tinsir <tinsir@foxmail.com>
2017-11-13 14:49:03 +0800

    day over

--- day1/ifelse.py
+++ day1/ifelse.py
@@ -5,3 +5,5 @@ age_num = int(age)
 
 if age_num > 18:
 	print('已经成年,年龄为%d'%age_num)
+else:
+	print('未成年,年龄为%d'%age_num)
--- day1/打印一个名片.py
+++ day1/打印一个名片.py
@@ -3,5 +3,6 @@
 name = input("请输入你的名字:")
 
 #使用print打印必要的消息
-
+print("*" * 10)
 print("你的名字是%s"%name)
+print("*" * 10)
